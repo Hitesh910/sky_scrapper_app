a7124e9fbaeff6559d2287cc2b8ad188157369d7
Hitesh <Hiteshkhatri910>
2024-07-23 23:39:56 +0530

    SkyScrapperApp

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:sky_scrapper_app/utils/app_routes.dart';
+
+void main()
+{
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,routes: app_routes,));
+}
